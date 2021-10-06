5f1138dc6662584e9a3c34db9ff03fc8589bba4e
ep34 <33396274+ep34@users.noreply.github.com>
2021-10-06 12:29:16 +0600

    Issue 26 resolved

---
+++ Custom Keyboard/Assets/CustomKeyboardLetC/Scripts/InputfieldPositionManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace LetC
+{
+    public class InputfieldPositionManager : MonoBehaviour
+    {
+        public RectTransform ui_panel;
+        public RectTransform inputField;
+
+        public void OnClick_Inputfield()
+        {
+            TextKeyboard.instance.isActive = true;
+            TextKeyboard.instance.activeInputField = inputField.GetComponent<TMPro.TMP_InputField>();
+            TextKeyboard.instance.OnClickDoneKey -= OnClick_DoneKey;
+            TextKeyboard.instance.OnClickDoneKey += OnClick_DoneKey;
+
+            if (inputField.anchoredPosition.y < TextKeyboard.instance.GetHeight())
+            {
+                ui_panel.anchoredPosition = new Vector2(ui_panel.anchoredPosition.x, TextKeyboard.instance.GetHeight() - inputField.anchoredPosition.y);
+            }
+        }
+
+        public void OnClick_DoneKey()
+        {
+            TextKeyboard.instance.isActive = false;
+            ui_panel.anchoredPosition = new Vector2(ui_panel.anchoredPosition.x, 0);
+        }
+    }
+}
+
